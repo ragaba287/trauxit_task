cd8db2ef6bbeb32e43ef120b23167cce82aa8dc0
Ahmed Ragab <ragaba267@gmail.com>
2023-08-23 21:47:57 +0300

    App Icon

---
+++ lib/res/res_images.dart
@@ -0,0 +1,5 @@
+class ResImages {
+  ///======================== Logo ==============================///
+  static const String logo = 'assets/images/trauxit_logo.png';
+  static const String fullLogo = 'assets/images/trauxit_bg.png';
+}
